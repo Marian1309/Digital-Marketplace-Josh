3737776b40e8bc358181320347d5fef9ce84af37
Marian <pidchashymaryan@gmail.com>
2023-11-27 07:54:16 +0200

    feat: created maxWidthWrapper component

---
+++ src/components/common/max-width-wrapper.tsx
@@ -0,0 +1,22 @@
+import type { FC, PropsWithChildren } from 'react';
+
+import { cn } from '@/lib/utils';
+
+type Props = {
+  className?: string;
+} & PropsWithChildren;
+
+const MaxWidthWrapper: FC<Props> = ({ className, children }) => {
+  return (
+    <div
+      className={cn(
+        'mx-auto w-full max-w-screen-xl px-2.5 md:px-20',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
